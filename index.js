70b1a57786b89bd8fb8bdd14e86dd269f12b2695
laden <njz198869@aliyun.com>
2019-07-12 12:32:18 +0800

    阅读源码

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// 配合parse2，将HTML代码转为ast
 var ElementType = require("domelementtype");
 
 var re_whitespace = /\s+/g;
@@ -5,6 +6,7 @@ var NodePrototype = require("./lib/node");
 var ElementPrototype = require("./lib/element");
 
 function DomHandler(callback, options, elementCB){
+	// 对重载处理，分为 DomHandler(callback: (error: Error, ast: any)=>void, options: object, elementCB: function)、DomHandler(options: object, elementCB: function)、 DomHandler(callback: function, elementCB: function)
 	if(typeof callback === "object"){
 		elementCB = options;
 		options = callback;
@@ -13,32 +15,41 @@ function DomHandler(callback, options, elementCB){
 		elementCB = options;
 		options = defaultOpts;
 	}
+
+	// 会有多次调用_callback的可能性吗？
 	this._callback = callback;
 	this._options = options || defaultOpts;
 	this._elementCB = elementCB;
+	// DOM树的数组
 	this.dom = [];
 	this._done = false;
+	// 当前DOM树的栈，会随着opentag和closetag进栈出栈。主要用于协助生成DOM树
 	this._tagStack = [];
 	this._parser = this._parser || null;
 }
 
 //default options
 var defaultOpts = {
+	// 是否应对文本节点中的空白进行规范化（所有空格都应替换为单个空格）。
 	normalizeWhitespace: false, //Replace all whitespace with single spaces
+	// 是否有startIndex
 	withStartIndices: false, //Add startIndex properties to nodes
+	// 是否有endIndex
 	withEndIndices: false, //Add endIndex properties to nodes
+	// 将DOM级别1属性添加到所有元素。
+	// withDomLvl1: false
 };
 
 DomHandler.prototype.onparserinit = function(parser){
 	this._parser = parser;
 };
 
-//Resets the handler back to starting state
+// 重新初始化一次
 DomHandler.prototype.onreset = function(){
 	DomHandler.call(this, this._callback, this._options, this._elementCB);
 };
 
-//Signals the handler that parsing is done
+// 结束
 DomHandler.prototype.onend = function(){
 	if(this._done) return;
 	this._done = true;
@@ -46,8 +57,10 @@ DomHandler.prototype.onend = function(){
 	this._handleCallback(null);
 };
 
+// 对错误处理
 DomHandler.prototype._handleCallback =
 DomHandler.prototype.onerror = function(error){
+	// 一旦发现错误，会立刻终吗？
 	if(typeof this._callback === "function"){
 		this._callback(error, this.dom);
 	} else {
@@ -67,6 +80,7 @@ DomHandler.prototype.onclosetag = function(){
 	if(this._elementCB) this._elementCB(elem);
 };
 
+// 创建element或者node的ast节点
 DomHandler.prototype._createDomElement = function(properties){
 	if (!this._options.withDomLvl1) return properties;
 
@@ -88,6 +102,7 @@ DomHandler.prototype._createDomElement = function(properties){
 
 DomHandler.prototype._addDomElement = function(element){
 	var parent = this._tagStack[this._tagStack.length - 1];
+	// 如果没有父节点，就直接创建新树
 	var siblings = parent ? parent.children : this.dom;
 	var previousSibling = siblings[siblings.length - 1];
 
@@ -111,6 +126,7 @@ DomHandler.prototype._addDomElement = function(element){
 	element.parent = parent || null;
 };
 
+// 打开标签
 DomHandler.prototype.onopentag = function(name, attribs){
 	var properties = {
 		type: name === "script" ? ElementType.Script : name === "style" ? ElementType.Style : ElementType.Tag,
@@ -119,10 +135,13 @@ DomHandler.prototype.onopentag = function(name, attribs){
 		children: []
 	};
 
+	// 创建一个element的节点对象
 	var element = this._createDomElement(properties);
 
+	// 加入当前DOM树中
 	this._addDomElement(element);
 
+	// 创建完会压栈
 	this._tagStack.push(element);
 };
 
@@ -134,6 +153,7 @@ DomHandler.prototype.ontext = function(data){
 	var lastTag;
 
 	if(!this._tagStack.length && this.dom.length && (lastTag = this.dom[this.dom.length-1]).type === ElementType.Text){
+		// 如果已经存在了一个文本节点，则在给文档节点中append新内容
 		if(normalize){
 			lastTag.data = (lastTag.data + data).replace(re_whitespace, " ");
 		} else {
@@ -152,6 +172,7 @@ DomHandler.prototype.ontext = function(data){
 				lastTag.data += data;
 			}
 		} else {
+			// 创建一个文本节点对象
 			if(normalize){
 				data = data.replace(re_whitespace, " ");
 			}
@@ -203,7 +224,7 @@ DomHandler.prototype.oncdatastart = function(){
 DomHandler.prototype.oncommentend = DomHandler.prototype.oncdataend = function(){
 	this._tagStack.pop();
 };
-
+// ???
 DomHandler.prototype.onprocessinginstruction = function(name, data){
 	var element = this._createDomElement({
 		name: name,
